eba859a3aac2fe239f4303b72b3bfe5eef790466
joao <hypergamerfrr@gmail.com>
2023-08-10 15:41:44 -0300

    att

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import Stepper from "./components/Stepper";
 
 export default function App() {
   return (
-   
-      <Stepper></Stepper>
+    <div className="bg-gray-900 flex flex-col gap-10 h-screen items-center justify-center">
+    <Stepper />
+  </div>
   )
 }
